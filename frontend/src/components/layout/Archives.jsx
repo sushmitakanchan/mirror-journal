3a2c7a06153e4bcd587817197af5134d021dd327
Sushmita <sushmitakanchan622@gmail.com>
2025-10-17 06:32:53 +0530

    rendering entries

--- frontend/src/components/layout/Archives.jsx
+++ frontend/src/components/layout/Archives.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Mails } from 'lucide-react';
+import { useEntries } from '@/context/EntriesContext';
 
 const Archives = () => {
+  const {entries} = useEntries()
   return (
     <div className='mx-50'>
       <div className="container py-4">
@@ -11,7 +13,18 @@ const Archives = () => {
               </Link>
       </div>
       <h1 className="text-5xl md:text-6xl gradient-title">Look back at your archives <Mails className='size-8  text-orange-500 inline-block'/> </h1>
-      <h2>Reflect on your thoughts with AI</h2>
+
+      <ul className='flex flex-col gap-2 mt-5'>
+      {entries.length > 0 ? entries.map((e, index)=>(
+        <div className='bg-gradient-to-br from-white/100 to-white/50 backdrop-blur-md rounded-xl shadow-lg border border-pink-300 p-3 max-w-3/4 h-35' key={index}>
+          <li className='flex flex-col items-start gap-2 ml-5'>
+            <h1 className='font-extrabold text-2xl'>{e.title}</h1>
+            <p>{e.mood}</p>
+            <p>{e.content}</p>
+          </li>
+        </div>
+      )):<h3 className='text-center text-gray-600 mt-10'>No enteries found.</h3>}
+      </ul>
     </div>
   )
 }
