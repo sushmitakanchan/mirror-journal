fabf916c66a72aec789d381a47768764cd9e05db
Sushmita <sushmitakanchan622@gmail.com>
2025-10-22 18:25:53 +0530

    footer enchancement

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-orange-200 py-12 bg-opacity-10 mt-20'>
     <div className='mx-auto px-4 text-center text-gray-900'>
-      FOOTER
+      FOOTER ENCHANCEMENT
     </div>
     </footer>
   )
